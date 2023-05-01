f77d86d8e9e850b44e6b8db33ad86d7565c7fb16
Yudi <renanyaguinuma@hotmail.com>
2023-05-01 20:46:50 -0300

    Add event list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script>
 	import Older from './index-components/older.svelte';
 	import Header from './index-components/header.svelte';
+	import List from './index-components/list.svelte';
 </script>
 
 <Header />
+
+<List />
 
 <Older />
---
+++ src/routes/index-components/list.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	//import secomppList from '../../../static/lista-secompps.json';
+
+	const url = './lista-secompps.json';
+
+	let secomppList: SecomppList[] = [];
+
+	onMount(async () => {
+		const response = await fetch(url);
+		secomppList = await response.json();
+	});
+
+	interface SecomppList {
+		name: string;
+		year: string;
+		date: string;
+		description: string;
+	}
+</script>
+
+{#each secomppList as item}
+	## {item.year}
+	**{item.name}**
+	{item.date}
+
+	{item.description}
+
+	- [Site](./{item.year})
+
+	<hr />
+{:else}
+	<p>Carregando...</p>
+{/each}
